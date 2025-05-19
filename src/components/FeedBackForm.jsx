338ec933d50e5dae2322531069b9699cb65303b5
Artaza <artazakhan556@gmail.com>
2025-05-19 21:42:49 +0530

    position-fixed

--- src/components/FeedBackForm.jsx
+++ src/components/FeedBackForm.jsx
@@ -53,7 +53,7 @@ const FeedBackForm = ({ dataArr, setDataArr, numOfGood, numOfBad, numOfAvg, setN
   return (
     <form
       onSubmit={handleSubmit(submitHandler)}
-      className="w-full py-2 px-6 flex flex-col justify-center items-center gap-4"
+      className="w-full py-2 px-6 flex flex-col justify-center items-center gap-4 relative"
     >
       <div className="flex justify-center items-center gap-4">
         <button
@@ -97,7 +97,7 @@ const FeedBackForm = ({ dataArr, setDataArr, numOfGood, numOfBad, numOfAvg, setN
         value="Submit Now"
         className="bg-gradient-to-r from-violet-500 via-30% to-sky-500 to-90% p-2 text-white font-bold cursor-pointer rounded w-[30%] shadow-lg shadow-sky-600/50 active:scale-[0.95]"
         />
-        <h2 className="text-sm opacity-75">Made by khan-artaza</h2>
+        <h2 className="text-sm opacity-75 absolute bottom-0">Made by khan-artaza</h2>
     </form>
   );
 };
